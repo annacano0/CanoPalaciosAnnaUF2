0f627cb0a7d4fc47b7422826d6a82591da344922
annacano0 <anna.elisabet5@gmail.com>
2025-03-25 19:24:37 +0100

    remove unused files

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,6 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/anys',
       name: 'anys',
 
