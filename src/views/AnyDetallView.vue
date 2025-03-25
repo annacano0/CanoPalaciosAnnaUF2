7745d704f34b12d8af3aea20dee6959c34a92031
annacano0 <anna.elisabet5@gmail.com>
2025-03-25 19:13:58 +0100

    add: calculatedPercentage

--- src/views/AnyDetallView.vue
+++ src/views/AnyDetallView.vue
@@ -1,32 +1,32 @@
 <script setup>
-import { useRouter } from "vue-router";
-import { useFetch } from "@/composables/useFetch";
-const router = useRouter();
+import { useRouter } from 'vue-router'
+import { useFetch } from '@/composables/useFetch'
+const router = useRouter()
 const goBack = () => {
-  router.push("/anys");
-};
+  router.push('/anys')
+}
 
 const { anyDetall } = defineProps({
-    anyDetall: String, 
-});
+  anyDetall: String,
+})
 
-const {getData, data} = useFetch(`?any=${anyDetall}`)
+const { getData, data } = useFetch(`?any=${anyDetall}`)
 getData()
 </script>
 <template>
-    <h2>Detall de l'any {{ anyDetall }}</h2>
-    <button @click="goBack">Enrere</button>
-    <ul v-if="data">
-        <li v-for="i in data" :key="i.tipus_de_centres">
-        <router-link
+  <h2>Detall de l'any {{ anyDetall }}</h2>
+  <button @click="goBack">Enrere</button>
+  <ul v-if="data">
+    <li v-for="i in data" :key="i.tipus_de_centres">
+      <router-link
         :to="{
-        
           name: 'centreDetall',
-          params: { anyDetall: i.any, detallCentre:'Empreses' },
+          params: { anyDetall: i.any, detallCentre: i.tipus_de_centres },
         }"
       >
         {{ i.tipus_de_centres }}
       </router-link>
-    </li>    </ul>
-    <p v-else>Dades no disponibles</p>
+    </li>
+  </ul>
+  <p v-else>Dades no disponibles</p>
 </template>
--- src/views/CentreDetallView.vue
+++ src/views/CentreDetallView.vue
@@ -6,24 +6,35 @@ const { anyDetall, detallCentre } = defineProps({
   anyDetall: String,
   detallCentre: String,
 })
-
-const { getData, data } = useFetch(`?any=${anyDetall}&&tipus_de_centres=${detallCentre}`)
+const filter = `?any=${anyDetall}&&tipus_de_centres=${detallCentre}`
+const { getData, data } = useFetch(filter)
 getData()
 
 const router = useRouter()
 const goBack = () => {
   router.push(`/detall_any/${anyDetall}`)
 }
-console.log(data)
+
+const getPercentages = (val1, val2) => {
+  const total = parseInt(val1) + parseInt(val2)
+  const res1 = (val1 * 100) / total
+  const res2 = (val2 * 100) / total
+
+  return [res1.toFixed(2), res2.toFixed(2)]
+}
+
+const pergentagesBeques = getPercentages(data.value[0].formaci_dones, data.value[0].formaci_homes)
+
+const pergentagesMov = getPercentages(data.value[0].mobilitat_dones, data.value[0].mobilitat_homes)
 </script>
 <template>
   <h2>Informe</h2>
   <button @click="goBack">Enrere</button>
   <ul v-if="data">
-    <li><span>Beques Dones: </span>{{ data[0].formaci_dones }} (%)</li>
-    <li><span>Beques Homes: </span>{{ data[0].formaci_homes }} (%)</li>
-    <li><span>Mobilitat Dones: </span>{{ data[0].mobilitat_dones }} (%)</li>
-    <li><span>Mobilitat Homes: </span>{{ data[0].mobilitat_homes }} (%)</li>
+    <li><span>Beques Dones: </span>{{ data[0].formaci_dones }} ({{ pergentagesBeques[0] }}%)</li>
+    <li><span>Beques Homes: </span>{{ data[0].formaci_homes }} ({{ pergentagesBeques[1] }}%)</li>
+    <li><span>Mobilitat Dones: </span>{{ data[0].mobilitat_dones }} ({{ pergentagesMov[0] }}%)</li>
+    <li><span>Mobilitat Homes: </span>{{ data[0].mobilitat_homes }} ({{ pergentagesMov[1] }}%)</li>
   </ul>
   <p v-else>Dades no disponibles</p>
 </template>
